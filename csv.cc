b08a92660dcc143b5b6c1eb9242e36b678121459
coreydunn <jumpingkanga@outlook.com>
2023-05-18 15:54:34 -0500

    comments

--- csv.cc
+++ csv.cc
@@ -4,15 +4,18 @@
 #include<stdbool.h>
 
 #include"csv.hh"
+#define CSV_MODE_NORMAL  0x0
+#define CSV_MODE_COMMENT 0x1
 
 std::vector<double>csv_read_floats(const char*fname)
 {
 	FILE*file;
-	std::vector<double>v;
+	int mode=CSV_MODE_NORMAL;
 	char buffer[65536]={0};
-	size_t n=0;
 	char float_buffer[1024]={0};
 	char*ptr=float_buffer;
+	size_t n=0;
+	std::vector<double>v;
 
 	file=fopen(fname,"r");
 	if(!file)return v;
@@ -21,14 +24,43 @@ std::vector<double>csv_read_floats(const char*fname)
 	for(size_t i=0;i<n;++i)
 	{
 
-		if(buffer[i]!=','&&buffer[i]!='\n'&&i<n-1)
-			*(ptr++)=buffer[i];
-		else
+		switch(mode)
 		{
-			v.push_back(strtof(float_buffer,NULL));
-			ptr=float_buffer;
-			memset(float_buffer,0,1024);
+
+			case CSV_MODE_COMMENT:
+				if(buffer[i]=='\n')
+					mode=CSV_MODE_NORMAL;
+				break;
+
+			case CSV_MODE_NORMAL:
+				if(buffer[i]=='#')
+					mode=CSV_MODE_COMMENT;
+				else if(buffer[i]!=','&&buffer[i]!='\n'&&i<n-1)
+					*(ptr++)=buffer[i];
+				else
+				{
+					v.push_back(strtof(float_buffer,NULL));
+					ptr=float_buffer;
+					memset(float_buffer,0,1024);
+				}
+				break;
+
 		}
+
+		// if(mode==CSV_MODE_COMMENT)
+		// 	if(buffer[i]=='\n')
+		// 		mode=CSV_MODE_NORMAL;
+		// else if(mode!=CSV_MODE_COMMENT&&buffer[i]=='#')
+		// 	mode=CSV_MODE_COMMENT;
+		// else if(buffer[i]!=','&&buffer[i]!='\n'&&i<n-1)
+		// 	*(ptr++)=buffer[i];
+		// else
+		// {
+		// 	v.push_back(strtof(float_buffer,NULL));
+		// 	ptr=float_buffer;
+		// 	memset(float_buffer,0,1024);
+		// }
+
 	}
 
 	fclose(file);
